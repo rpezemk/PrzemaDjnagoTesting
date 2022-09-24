caf2cfd7a6d4f8214c62f00b679331e80ce736b2
rpezemk <rpezemk@gmail.com>
2022-09-25 00:20:00 +0200

    asdf

--- ErrorsManager/ErrorBrowser/views.py
+++ ErrorsManager/ErrorBrowser/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def doc_view(request, *args, **kwargs):
+    print(request)
+    print(request.user)
+    print(args)
+    print(kwargs)
+    return render(request, "docTemplate.html", {})
+
+
+    
